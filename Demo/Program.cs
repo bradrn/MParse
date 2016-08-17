bf043a683e12bd2b79cdd24ae60b32d2cb76ebf2
bradrn <brad.neimann@hotmail.com>
2016-08-17 18:25:32 +1000

    Delete unneeded classes

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -12,7 +12,6 @@ using ParseState = CSFunc.Types.Error<System.Tuple<System.Collections.Immutable.
 using NonTerminal = System.Func<CSFunc.Types.Error<System.Tuple<System.Collections.Immutable.ImmutableList<MParse.Lexer.Token>, System.Collections.Immutable.ImmutableList<MParse.Lexer.Token>, MParse.Parser.Tree<MParse.Parser.Term>>, MParse.Parser.ParseError>,
                                 CSFunc.Types.Error<System.Tuple<System.Collections.Immutable.ImmutableList<MParse.Lexer.Token>, System.Collections.Immutable.ImmutableList<MParse.Lexer.Token>, MParse.Parser.Tree<MParse.Parser.Term>>, MParse.Parser.ParseError>>;
 using AST = MParse.Parser.Tree<MParse.Parser.Term>;
-using T = MParse.Parser.TermSpecification;
 using TSpec = System.Collections.Generic.KeyValuePair<string, int>;
 
 using static MParse.Parser.Parser;
