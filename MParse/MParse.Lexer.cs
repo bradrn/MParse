72518a6617ed2d310730d327beda058fdc6bdb2d
bradrn <brad.neimann@hotmail.com>
2016-09-07 16:24:39 +1000

    Create NFA.ToDFA method

--- MParse/MParse.Lexer.cs
+++ MParse/MParse.Lexer.cs
@@ -44,6 +44,91 @@ namespace MParse.Lexer
             StartingState = startingState;
             AcceptingStates = acceptingStates;
         }
+        public DFA ToDFA()
+        {
+            /* A DFA is created from an NFA in two steps:
+             
+                 (1) Construct a DFA whose each of whose states is composite,
+                     namely a list of NFA states.
+             
+                 (2) Replace composite states (List of int) by simple states
+                     (int).
+               (https://www.itu.dk/people/sestoft/gcsharp/GNfaToDfa.cs)
+             */
+
+            Func<List<int>, List<int>, bool> equalUnordered = (l1, l2) => Enumerable.SequenceEqual(l1.OrderBy(i => i), l2.OrderBy(i => i));
+
+            // STEP 1
+            Dictionary<List<int>, Dictionary<char, List<int>>> compositeDFA = new Dictionary<List<int>, Dictionary<char, List<int>>>();
+            Queue<List<int>> compositeStatesNotDone = new Queue<List<int>>(); // Keeps track of composite states that have not yet been added to the DFA
+
+            Dictionary<char, List<int>> startingStateClosure = CloseState(StartingState);
+            compositeDFA.Add(new List<int> { StartingState }, startingStateClosure);
+            foreach (KeyValuePair<char, List<int>> kvp in startingStateClosure)
+                if (!compositeStatesNotDone.Contains(kvp.Value)) compositeStatesNotDone.Enqueue(kvp.Value);
+            
+            while (compositeStatesNotDone.Count != 0)
+            {
+                List<int> curCompositeState = compositeStatesNotDone.Dequeue();
+                Dictionary<char, List<int>> curCompositeStateClosure = CloseState(curCompositeState);
+                compositeDFA.Add(curCompositeState, curCompositeStateClosure);
+                foreach (KeyValuePair<char, List<int>> kvp in curCompositeStateClosure)
+                    if (!compositeStatesNotDone.Any(l => equalUnordered(l, kvp.Value)) &&   // If the state has not already been added to compositeStatesNotDone
+                        !compositeDFA.Keys.Any(l => equalUnordered(l, kvp.Value)))          // and if it is not already in compositeDFA
+                        compositeStatesNotDone.Enqueue(kvp.Value);
+            }
+
+            // STEP 2
+            Dictionary<List<int>, int> mappings = new Dictionary<List<int>, int>();
+            int curState = 0;
+            foreach (KeyValuePair<List<int>, Dictionary<char, List<int>>> compositeTransitions in compositeDFA)
+            {
+                if (!mappings.Any(mapping => equalUnordered(mapping.Key, compositeTransitions.Key)))
+                {
+                    mappings.Add(compositeTransitions.Key, curState);
+                    curState++;
+                }
+            }
+
+            DFA dfa = new DFA(new Dictionary<int, Dictionary<char, int>>(), new List<int>(), StartingState);
+            Func<List<int>, int> getNum = l => mappings.First(kvp => equalUnordered(kvp.Key, l)).Value;
+            foreach (KeyValuePair<List<int>, Dictionary<char, List<int>>> compositeTransitions in compositeDFA)
+            {
+                dfa.StateTable.Add(getNum(compositeTransitions.Key), compositeTransitions.Value.ToDictionary(kvp => kvp.Key, kvp => getNum(kvp.Value)));
+                if (compositeTransitions.Value.Count == 1 && compositeTransitions.Key.Contains(StartingState)) dfa.StartingState = getNum(compositeTransitions.Key);
+                if (compositeTransitions.Key.Intersect(AcceptingStates).Any()) dfa.AcceptingStates.Add(getNum(compositeTransitions.Key));
+            }
+            return dfa;
+        }
+        private Dictionary<char, List<int>> CloseState(int state)
+        {
+            Dictionary<char, List<int>> combinedTransitions = new Dictionary<char, List<int>>();
+            foreach (KeyValuePair<char, int> transition in StateTable[state])
+            {
+                if (combinedTransitions.Select(kvp => kvp.Key).Contains(transition.Key))
+                {
+                    if (!combinedTransitions.First(kvp => kvp.Key == transition.Key).Value.Contains(transition.Value))
+                        combinedTransitions.First(kvp => kvp.Key == transition.Key).Value.Add(transition.Value);
+                }
+                else combinedTransitions.Add(transition.Key, new List<int> { transition.Value });
+            }
+            return combinedTransitions;
+        }
+        private Dictionary<char, List<int>> CloseState(List<int> combinedState)
+        {
+            Dictionary<char, List<int>> combinedTransitions = new Dictionary<char, List<int>>();
+            foreach (int state in combinedState)
+            {
+                // Merge CloseState(state) with combinedTransitions
+                CloseState(state).ToList().ForEach(kvp =>
+                {
+                    if (combinedTransitions.ContainsKey(kvp.Key)) combinedTransitions.First(_kvp => _kvp.Key == kvp.Key).Value.AddRange(kvp.Value);
+                    else                                          combinedTransitions.Add(kvp.Key, kvp.Value);
+                });
+                combinedTransitions = combinedTransitions.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.Distinct().ToList()); // Remove duplicates in values
+            }
+            return combinedTransitions;
+        }
     }
 
     public class eNFA
