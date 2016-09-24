c8e50cdadad32166ca3622e24c60889ebc53fb0e
bradrn <brad.neimann@hotmail.com>
2016-09-24 19:06:39 +1000

    Add ParserTest class

---
+++ Tests/ParserTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSFunc.Types;
+using MParse.Parser;
+using MParse.Lexer;
+
+// Using statements required for MParse.Parser
+using ParseState = CSFunc.Types.Error<System.Tuple<System.Collections.Immutable.ImmutableList<MParse.Lexer.Token>, System.Collections.Immutable.ImmutableList<MParse.Lexer.Token>, MParse.Parser.Tree<MParse.Parser.Term>>, MParse.Parser.ParseError>;
+using NonTerminal = System.Func<CSFunc.Types.Error<System.Tuple<System.Collections.Immutable.ImmutableList<MParse.Lexer.Token>, System.Collections.Immutable.ImmutableList<MParse.Lexer.Token>, MParse.Parser.Tree<MParse.Parser.Term>>, MParse.Parser.ParseError>,
+                                CSFunc.Types.Error<System.Tuple<System.Collections.Immutable.ImmutableList<MParse.Lexer.Token>, System.Collections.Immutable.ImmutableList<MParse.Lexer.Token>, MParse.Parser.Tree<MParse.Parser.Term>>, MParse.Parser.ParseError>>;
+using AST = MParse.Parser.Tree<MParse.Parser.Term>;
+using TSpec = System.Collections.Generic.KeyValuePair<string, int>;
+
+namespace Tests
+{
+    [TestClass]
+    public class ParserTest
+    {
+    }
+}
