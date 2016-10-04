0beaa4832abd01fb48b48341a68a037452ec25e2
bradrn <brad.neimann@hotmail.com>
2016-10-04 18:29:03 +1100

    Add comments

--- Tests/ParserTest.cs
+++ Tests/ParserTest.cs
@@ -23,10 +23,16 @@ namespace Tests
         {
             NonTerminal nt = text => text.Parse(0).Rule(0);
             Error<AST, ParseError> result = Parser.DoParse(nt, ImmutableList.Create(new Token(0, "token", new SampleLocation())));
+
+            // Test if parse succeeded
             Assert.IsTrue(result.State == ErrorState.Result);
+
+            // Test if root of AST is a nonterminal with rulenumber 0
             Assert.IsTrue(result.Match(Result: ast => ast.Value.NonTerminalOrDefault(NonTerminal: _nt => _nt == 0,
                                                                                      Default: false),
                                        Throw: perr => false));
+
+            // Test if first child of AST is a nonterminal of type 0 and with value "token"
             Assert.IsTrue(result.Match(Result: ast => ast.Children[0].Value.TerminalOrDefault(Terminal: tok => tok.Value == "token" && tok.Type == 0,
                                                                                               Default: false),
                                        Throw: perr => false));
@@ -37,14 +43,26 @@ namespace Tests
         {
             NonTerminal nt = text => text.Parse(0).Rule(0);
             Error<AST, ParseError> result = Parser.DoParse(nt, ImmutableList.Create(new Token(1, "token", new SampleLocation())));
+
+            // Test if parse failed
             Assert.IsTrue(result.State == ErrorState.Throw);
+
+            // Test if state of error.Expected is ParseError.ExpectedValueState.Token
             Assert.IsTrue(result.Match(Result: ast => false, Throw: perr => (perr.Expected.State == ParseError.ExpectedValueState.Token)));
+
+            // Test if error.Expected is a token of type 0
             Assert.IsTrue(result.Match(Result: ast => false, Throw: perr => (perr.Expected.TokenOrDefault(Token: tok => tok == 0,
                                                                                                           Default: false))));
+
+            // Test if state of error.Got is ParseError.GotValueState.Token
             Assert.IsTrue(result.Match(Result: ast => false, Throw: perr => (perr.Got.State == ParseError.GotValueState.Token)));
+
+            // Test if error.Got is a token of type 1 and with value "token"
             Assert.IsTrue(result.Match(Result: ast => false, Throw: perr => (perr.Got.TokenOrDefault(Token: tok => tok.Type == 1 && tok.Value == "token",
                                                                                                      Default: false))));
-            Assert.IsTrue(result.Match(Result: ast => false, Throw: perr => perr.Location is SampleLocation));  // Can't say `perr.Location == new SampleLocation()
+            
+            // Test if the location of the error is correct
+            Assert.IsTrue(result.Match(Result: ast => false, Throw: perr => perr.Location is SampleLocation));  // Can't say `perr.Location == new SampleLocation()`
                                                                                                                 // because equality is by reference
         }
 
@@ -54,13 +72,21 @@ namespace Tests
             NonTerminal nt = text => text.Parse(0).Parse(1).Rule(0);
             Error<AST, ParseError> result = Parser.DoParse(nt, ImmutableList.Create(new Token(0, "token0", new SampleLocation()),
                                                                                     new Token(1, "token1", new SampleLocation())));
+
+            // Test if parse succeeded
             Assert.IsTrue(result.State == ErrorState.Result);
+
+            // Test if root of AST is a nonterminal with rulenumber 0
             Assert.IsTrue(result.Match(Result: ast => ast.Value.NonTerminalOrDefault(NonTerminal: _nt => _nt == 0,
                                                                                      Default: false),
                                        Throw: perr => false));
+
+            // Test if first child of root is a terminal of type 0 with value "token0"
             Assert.IsTrue(result.Match(Result: ast => ast.Children[0].Value.TerminalOrDefault(Terminal: tok => tok.Value == "token0" && tok.Type == 0,
                                                                                               Default: false),
                                        Throw: perr => false));
+
+            // Test if second child of root is a terminal of type 1 with value "token1"
             Assert.IsTrue(result.Match(Result: ast => ast.Children[1].Value.TerminalOrDefault(Terminal: tok => tok.Value == "token1" && tok.Type == 1,
                                                                                               Default: false),
                                        Throw: perr => false));
@@ -72,11 +98,21 @@ namespace Tests
             NonTerminal nt = text => text.Parse(0).Parse(1).Rule(0);
             Error<AST, ParseError> result = Parser.DoParse(nt, ImmutableList.Create(new Token(0, "token0-0", new SampleLocation()),
                                                                                     new Token(0, "token0-1", new SampleLocation())));
+
+            // Test if parse failed
             Assert.IsTrue(result.State == ErrorState.Throw);
+
+            // Test if state of error.Expected is ParseError.ExpectedValueState.Token
             Assert.IsTrue(result.Match(Result: ast => false, Throw: perr => perr.Expected.State == ParseError.ExpectedValueState.Token));
+
+            // Test if error.Expected is a token of type 1
             Assert.IsTrue(result.Match(Result: ast => false, Throw: perr => perr.Expected.TokenOrDefault(Token: tok => tok == 1,
                                                                                                          Default: false)));
+
+            // Test if state of error.Got is ParseError.GotValueState.Token
             Assert.IsTrue(result.Match(Result: ast => false, Throw: perr => perr.Got.State == ParseError.GotValueState.Token));
+
+            // Test if error.Got is a token of type 0 with value "token0-1"
             Assert.IsTrue(result.Match(Result: ast => false, Throw: perr => perr.Got.TokenOrDefault(Token: tok => tok.Type == 0 && tok.Value == "token0-1",
                                                                                                     Default: false)));
         }
@@ -89,24 +125,37 @@ namespace Tests
             NonTerminal nt1 = text => text.Parse(nt2).Parse(nt2).Rule(0);
             Error<AST, ParseError> result = Parser.DoParse(nt1, ImmutableList.Create(new Token(0, "token0", new SampleLocation()),
                                                                                      new Token(0, "token1", new SampleLocation())));
+
+            // Test if parse succeeded
             Assert.IsTrue(result.State == ErrorState.Result);
+
+            // Test if root of AST is a nonterminal with rulenumber 0
             Assert.IsTrue(result.Match(Result: ast => ast.Value.NonTerminalOrDefault(NonTerminal: nt => nt == 0,
                                                                                      Default: false),
                                        Throw: perr => false));
+            
+            // Test if first child of root is a nonterminal
             Assert.IsTrue(result.Match(Result: ast => ast.Children[0].Value.State == TermState.NonTerminal, Throw: perr => false));
+
+            // Test if first child of root is a nonterminal with rulenumber 1
             Assert.IsTrue(result.Match(Result: ast => ast.Children[0].Value.NonTerminalOrDefault(NonTerminal: nt => nt == 1,
                                                                                                  Default: false),
                                        Throw: perr => false));
+
+            // Test if second child of root is a nonterminal with rulenumber 1
             Assert.IsTrue(result.Match(Result: ast => ast.Children[1].Value.NonTerminalOrDefault(NonTerminal: nt => nt == 1,
                                                                                                  Default: false),
                                        Throw: perr => false));
+
+            // Test if first child of first child of root is a terminal of type 0 with value "token0"
             Assert.IsTrue(result.Match(Result: ast => ast.Children[0].Children[0].Value.TerminalOrDefault(Terminal: tok => tok.Type == 0 & tok.Value == "token0",
                                                                                                           Default: false),
                                        Throw: perr => false));
+
+            // Test if first child of second child of root is a terminal of type 0 with value "token1"
             Assert.IsTrue(result.Match(Result: ast => ast.Children[1].Children[0].Value.TerminalOrDefault(Terminal: tok => tok.Type == 0 & tok.Value == "token1",
                                                                                                           Default: false),
                                        Throw: perr => false));
-
         }
 
         class SampleLocation : ILocation { }
